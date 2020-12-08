ed84e68b62aa41c546c213fdd3ea265aecda4fba
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2020-12-08 19:01:08 +0530

    fix:create session

--- api/serializers.py
+++ api/serializers.py
@@ -11,3 +11,10 @@ class RoomSerializer(serializers.ModelSerializer):
             'votes_to_skip',
             'created_at'
         )
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Room
+        fields=(
+            'guest_can_pause',
+            'votes_to_skip'
+        )
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from .views import RoomView
+from .views import CreateRoomView
 urlpatterns = [
-    path('room',RoomView.as_view())
+    path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
