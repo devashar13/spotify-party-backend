8a68aea19d4eedecbc42e0ff7fcdd8b4f5b89702
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2020-12-15 15:10:52 +0530

    Feat:Get room info

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .views import RoomView
-from .views import CreateRoomView
+from .views import CreateRoomView,GetRoom
+
 urlpatterns = [
     path('room', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room',GetRoom.as_view())
 ]
--- spotify_backend/settings.py
+++ spotify_backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+CORS_ALLOWED_ORIGINS = [
+    
+    "http://localhost:8080",
+    "http://localhost:3000"
 ]
 
 ROOT_URLCONF = 'spotify_backend.urls'
